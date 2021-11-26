a3df9c96959d4f73b19f69bd68ca9ecde3dbdfd3
MysterioPRM <79892747+MysterioPRM@users.noreply.github.com>
2021-11-26 18:56:12 +0500

    server core
    server core

--- sql/LatincoreBfa/world/2020_07_17_06_fix_crash_mechagnome.sql
+++
@@ -1,8 +0,0 @@
-REPLACE INTO `disables` VALUES
-(0,312923,9,0,0,'mechagnome pasive'),
-(0,312896,9,0,0,'mechagnome pasive'),
-(0,312890,9,0,0,'mechagnome pasive'),
-(0,312916,9,0,0,'mechagnome pasive'),
-(0,312924,9,0,0,'mechagnome pasive');
-
-update `race_unlock_requirement` set `expansion`=0 where `raceid`=37;
